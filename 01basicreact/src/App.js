a08f1a1418fd6e31d5752388025d48063fee00aa
Ankesh Kumar <ankeshkumar822@gmail.com>
2024-06-08 17:48:21 +0530

    2 react project is created

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+  <h1>Hello Ankesh Welcome to react_js</h1>
+  );
+}
+
+export default App;
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,6 @@
+function App() {
+
+  return <h1>Hello Ankesh | Welcome to React Vite Project</h1>;
+}
+
+export default App;
